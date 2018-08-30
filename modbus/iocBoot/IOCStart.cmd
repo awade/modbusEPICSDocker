e5cae813097ef22fcfad12621adff85bd437be8d
Andrew Wade <andrew.richard.wade@gmail.com>
2018-08-29 18:52:35 -0700

    Change location of default test db file.

--- modbus/iocBoot/IOCStart.cmd
+++ modbus/iocBoot/IOCStart.cmd
@@ -1,6 +1,6 @@
 # This is a minimum working example of a .cmd file for modbus over TCP/IP
 # server. This is configured for a single soft channel.  To enable an acromag
-# XT1211, XT1541, 966en-4006 or XT1112, then uncomment those lines below 
+# XT1211, XT1541, 966en-4006 or XT1112, then uncomment those lines below
 # and enter correct IP addresses for those units. You will need to add entries
 # into the .db files in the ../db folder yourself.
 
@@ -24,9 +24,9 @@ modbus_registerRecordDeviceDriver(pdbbase)
 #### TEST XT1112 ####
 #drvAsynIPPortConfigure("xt1112test1","10.0.1.46:502",0,0,1)
 #modbusInterposeConfig("xt1112test1",0,5000,0)
-#drvModbusAsynConfigure("BIO_Reg_2","xt1112test1",0,6,0,4,0,0,"Acromag") 
+#drvModbusAsynConfigure("BIO_Reg_2","xt1112test1",0,6,0,4,0,0,"Acromag")
 
 
-dbLoadDatabase("/home/modbus/db/testDefault.db")
+dbLoadDatabase("/home/modbus/testDefault.db")
 
 iocInit
